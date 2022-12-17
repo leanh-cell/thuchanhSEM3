52341a93a8709165cd592c45c3f074ca0c56eb3e
leanh-cell <67904136+leanh-cell@users.noreply.github.com>
2022-12-17 18:51:39 +0700

    hoanthanh

---
+++ MvcKiemtra/Data/MvcKiemtraContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcKiemtra.Models;
+
+namespace MvcKiemtra.Data
+{
+    public class MvcKiemtraContext : DbContext
+    {
+        public MvcKiemtraContext (DbContextOptions<MvcKiemtraContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcKiemtra.Models.Employee> Employee { get; set; } = default!;
+    }
+}
